4d01370660342b838c136ea11e378a071c40314a
heavenwill <magicalundeadunsizedcarp@gmail.com>
2017-03-30 01:05:18 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Map{
+	class Tile{
+		int value;
+		bool 
+	};
+	int size_x;
+    Tile **array;
+public:
+    Map(int y, int x){
+    	array=new *Tile	[y];
+    	for (int i = 0; i < x; i ++) t[i] = new Kletka[n];
+	}
+	~Map(){
+		
+	}
+	
+};
+
+int main(){
+	return 0;
+}
