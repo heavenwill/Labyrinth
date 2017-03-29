eacb2ad0e595a57173b0131f4cda4b9dfbe542a0
morozyto <moranton@mail.ru>
2017-03-30 01:10:28 +0300

    firstUpdate
    forFun

--- main.cpp
+++ main.cpp
@@ -2,18 +2,24 @@
 
 class Map{
 	class Tile{
-		int value;
-		bool 
+	private:
+			int value;
+			bool up = false;
+			bool down = false;
+			bool left = false;
+			bool right = false;
 	};
+private:
 	int size_x;
-    Tile **array;
+    	Tile **array;
 public:
-    Map(int y, int x){
-    	array=new *Tile	[y];
-    	for (int i = 0; i < x; i ++) t[i] = new Tile[n];
+	Map(int y, int x){
+    		array = new *Tile	[y];
+    		for (int i = 0; i < x; i++) t[i] = new Tile[n];
 	}
+	
 	~Map(){
-		delete [] array;
+		
 	}
 	
 };
